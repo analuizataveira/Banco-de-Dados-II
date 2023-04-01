41d348bece9154bcb5366d41a92f88eeb61e68ff
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2023-04-01 10:46:55 -0300

    Aggregation Pipelines

---
+++ Relatorio4/main.py
@@ -0,0 +1,8 @@
+from productAnalyzer import ProductAnalyzer
+
+productAnalyzer = ProductAnalyzer("mercado", "compras")
+
+productAnalyzer.totalVendas()
+productAnalyzer.produtoMaisVendido()
+productAnalyzer.clienteMaisGastou()
+productAnalyzer.maisDeUmProdutoVendido()
