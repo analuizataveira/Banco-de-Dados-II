b665493a807334ab6429034e595b7035e96e4642
Ana Luiza <106170960+analuizataveira@users.noreply.github.com>
2023-04-01 10:45:48 -0300

    Delete Relatorio4 directory

--- Relatorio4/main.py
+++
@@ -1,8 +0,0 @@
-from productAnalyzer import ProductAnalyzer
-
-productAnalyzer = ProductAnalyzer("mercado", "compras")
-
-#productAnalyzer.totalVendas()
-#productAnalyzer.produtoMaisVendido()
-#productAnalyzer.clienteMaisGastou()
-productAnalyzer.maisDeUmProdutoVendido()
